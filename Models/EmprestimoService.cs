d392e365c0dd40d1c6eb9b3c0dcd4c021ac30927
Lucas <lpavani2011@hotmail.com>
2021-09-06 02:47:38 -0300

    listagem emprestimos arrumada

--- Models/EmprestimoService.cs
+++ Models/EmprestimoService.cs
@@ -30,11 +30,43 @@ namespace Biblioteca.Models
             }
         }
 
-        public ICollection<Emprestimo> ListarTodos(FiltrosEmprestimos filtro)
+        public ICollection<Emprestimo> ListarTodos(FiltrosEmprestimos filtro=null)
         {
             using(BibliotecaContext bc = new BibliotecaContext())
             {
-                return bc.Emprestimos.Include(e => e.Livro).ToList();
+                IQueryable<Emprestimo> query;
+
+                if(filtro!=null){
+                    switch(filtro.TipoFiltro){
+                        case "Livro":
+                        query = bc.Emprestimos.Where(e=>e.Livro.Autor.Contains(filtro.Filtro)|e.Livro.Titulo.Contains(filtro.Filtro));
+                        
+                        break;
+                        
+                        case "Usuario":
+                        query= bc.Emprestimos.Where(e=>e.NomeUsuario.Contains(filtro.Filtro));
+
+                        break;
+
+                        default: 
+                        query = bc.Emprestimos;
+                        break;
+
+                        
+
+
+
+
+                    }
+                }
+
+                else{
+                    query = bc.Emprestimos;
+                }
+
+                return query
+                .OrderBy(e=>e.DataDevolucao)
+                .Include(e => e.Livro).ToList();
             }
         }
 
